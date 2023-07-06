0b17907af68f68f14a7089bcea766102dec9c71e
rafae2 <126732175+rafae2@users.noreply.github.com>
2023-07-05 20:46:19 -0400

    Add files via upload
    Não consegui deixar responsivo, mais irei trabalhar e estudar mais ainda para conseguir

---
+++ Potifófio 2/portfólio2.js
@@ -0,0 +1,8 @@
+function abrirmenu(){
+let barraMenuAberto = document.getElementById("barra-menu");
+if (barraMenuAberto.style.width == '0px'){
+(barraMenuAberto.style.width ='180px');
+}else{
+     barraMenuAberto.style.width = '0px'
+}
+}
